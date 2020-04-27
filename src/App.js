0088b038f4354b8f07c0c5b6413a7199ae92a40d
Dan Halperin <dee.aye.en@gmail.com>
2020-04-27 05:38:42 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import "./App.css";
 
 function App() {
+  const section1Ref = useRef(null);
+  const section2Ref = useRef(null);
+  const section3Ref = useRef(null);
+
+  const scrollTo = (ref) => {
+    window.scroll({
+      top: ref.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundColor: "#282c34",
+        color: "white",
+        textAlign: "center",
+      }}
+    >
+      <div style={{ position: "fixed", top: "16px", left: "16px" }}>
+        <button onClick={() => scrollTo(section1Ref)}>Section 1</button>
+        <br />
+        <button onClick={() => scrollTo(section2Ref)}>Section 2</button>
+        <br />
+        <button onClick={() => scrollTo(section3Ref)}>Section 3</button>
+      </div>
+      <h1 ref={section1Ref} style={{ height: "100vh", margin: 0 }}>
+        Section 1
+      </h1>
+      <h1 ref={section2Ref} style={{ height: "100vh", margin: 0 }}>
+        Section 2
+      </h1>
+      <h1 ref={section3Ref} style={{ height: "100vh", margin: 0 }}>
+        Section 3
+      </h1>
     </div>
   );
 }
